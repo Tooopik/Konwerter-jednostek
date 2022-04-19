7d96f19cafe930b82261ba1b16d9e272f31a95db
Adrian Wójcik <adrian.wojcik00@gmail.com>
2022-04-19 22:38:17 +0200

    Add file units.py and move units definitions to it

--- Convert_unit.py
+++ Convert_unit.py
@@ -1,29 +1,12 @@
-units = {1:
-         {'km': {'m': 1000, 'cm': 100000, 'mm': 1000000, 'nm': 0.54},
-          'm': {'km': 0.001, 'cm': 100, 'mm': 1000, 'nm': 0.0054}},
-         2:
-         {'kg': {'g': 1000, 'dkg': 100, 'oz': 35.27},
-          'oz': {'kg': 0.03, 'g': 28.35}}}
-
-unitsTypes = {1: 'Długośc',
-              2: 'Waga'}
-
-unitsNames = {'km': 'kilometry',
-              'm': 'metry',
-              'cm': 'centymetry',
-              'mm': 'milimetry',
-              'nm': 'mile morskie',
-              'kg': 'kilogram',
-              'g': 'gram',
-              'dkg': 'dekagram',
-              'oz': 'uncja'}
+from units import units
+from units import unitsTypes
+from units import unitsNames
 
 print('Wybierz cyfre odpowiadającą typowi jednostki do przeliczenia')
 for i in unitsTypes:
     value = int(i)
     print(f'{value} : {unitsTypes[value]}')
 mainUnitType = int(input('>>> '))
-
 
 print('Wybierz jednostkę do przeliczenia')
 for name in units[mainUnitType]:
